42a2e3bd8f7b7432290404d73b546a848a7edd3d
Jianyu.Wang <873869027@qq.com>
2018-10-09 22:35:04 +0800

    training version

--- train/abnormal_detection/available_memory.py
+++ train/abnormal_detection/available_memory.py
@@ -1,41 +1,59 @@
-from utils.read_csv import Reader
-from utils.lstm_model import lstm_model
+from utils.preprocessing import Data
+import utils.lstm_model
 
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
+# settings of lstm model
 timesteps = 20
-epochs = 100
 batch_size = 64
+epochs = 100
+
+lstm_size = 30
+lstm_layers = 2
+
+filename = "../../datasets/db_os_stat(180123-180128).csv"
+column = "FREE_MEM_SIZE"
 
 
 def train_model():
-    # define training process
-    x = tf.placeholder(tf.float32, [None, timesteps, 1], name='input_x')
-    y_ = tf.placeholder(tf.float32, [None, 1], name='input_y')
-    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
-    # loading lstm model
-    predictions = lstm_model(x, keep_prob)
-    # mse loss function
-    cost = tf.losses.mean_squared_error(y_, predictions)
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
-
     # prepare data
-    reader = Reader("../../datasets/db_os_stat(180123-180128).csv", "FREE_MEM_SIZE")
+    data = Data("../../datasets/db_os_stat(180123-180128).csv", "FREE_MEM_SIZE", timesteps=timesteps)
+    # init placeholder
+    x, y, keep_prob = utils.lstm_model.initPlaceholder(timesteps)
+    predictions = utils.lstm_model.lstm_model(x,
+                                              lstm_size=lstm_size,
+                                              lstm_layers=lstm_layers,
+                                              keep_prob=keep_prob)
+    # mse loss function
+    cost = tf.losses.mean_squared_error(y, predictions)
+    # optimizer
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
     # define session
     gpu_options = tf.GPUOptions(allow_growth=True)
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
         tf.global_variables_initializer().run()
+        # batches counter
         iteration = 1
-        for e in range(epochs):
-            for xs, ys in reader.getBatch(reader.train_x, reader.train_y, batch_size):
-                feed_dict = {x: xs[:, :, None], y_: ys[:, None], keep_prob: .5}
+        # loop for training
+        for epoch in range(epochs):
+            for xs, ys in data.getBatches(data.train_x, data.train_y, batch_size):
+                feed_dict = {x: xs[:, :, None], y: ys[:, None], keep_prob: .5}
                 loss, train_step = sess.run([cost, optimizer], feed_dict=feed_dict)
+
                 if iteration % 100 == 0:
-                    print('Epochs:{}/{}'.format(e, epochs),
+                    print('Epochs:{}/{}'.format(epoch, epochs),
                           'Iteration:{}'.format(iteration),
                           'Train loss: {}'.format(loss))
-            iteration += 1
+                iteration += 1
+        # test model
+        feed_dict = {x: data.test_x[:, :, None], keep_prob: 1.0}
+        results = sess.run(predictions, feed_dict=feed_dict)
+        plt.plot(results, 'r', label='predicted')
+        plt.plot(data.test_y, 'g--', label='real')
+        plt.legend()
+        plt.show()
 
 
 if __name__ == "__main__":
--- utils/lstm_model.py
+++ utils/lstm_model.py
@@ -1,18 +1,25 @@
 import tensorflow as tf
 
-lstm_size = 32
-lstm_layers = 2
+
+def initPlaceholder(timesteps):
+    x = tf.placeholder(tf.float32, [None, timesteps, 1], name='input_x')
+    y_ = tf.placeholder(tf.float32, [None, 1], name='input_y')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    return x, y_, keep_prob
 
 
-def lstm_model(X, keep_prob):
-    # define basic cell
-    def lstmCell():
+def lstm_model(x, lstm_size, lstm_layers, keep_prob):
+    # define basis structure LSTM cell
+    def lstm_cell():
         lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
         drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob)
         return drop
-    # define multi cell
-    cell = tf.contrib.rnn.MultiRNNCell([lstmCell() for _ in range(lstm_layers)])
-    outputs, final_state = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+    # multi layer LSTM cell
+    cell = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(lstm_layers)])
+    # dynamic rnn
+    outputs, final_state = tf.nn.dynamic_rnn(cell, x, dtype=tf.float32)
+    # reverse
     outputs = outputs[:, -1]
+    # fully connected
     predictions = tf.contrib.layers.fully_connected(outputs, 1, activation_fn=tf.sigmoid)
     return predictions
