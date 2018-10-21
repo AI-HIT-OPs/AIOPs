5e3f8a5aba17b95d340176772339b6bf00610d5d
Jianyu.Wang <873869027@qq.com>
2018-10-21 21:45:26 +0800

    import apscheduler timed task control.

--- test/timed_tasks/abnormal_detection.py
+++ test/timed_tasks/abnormal_detection.py
@@ -13,10 +13,28 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def tick():
     print('Tick! The time is: %s' % datetime.now())
 
+
 def tick1():
     print('Tick! The time1 is: %s' % datetime.now())
 
 if __name__ == '__main__':
+
+    # settings
+    timesteps = 20
+    batch_size = 64
+
+    filename_available_memory = "../../datasets/db_os_stat(180123-180128).csv"
+    filename_consistent_gets = "../../datasets/db_stat(180123-180128).csv"
+    model_available_memory = "../../model/abnormal_detection_model/available_memory_model/FREE_MEM_SIZE_MODEL"
+    model_consistent_gets = "../../model/abnormal_detection_model/consistent_gets_model/CONSISTENT_GETS_MODEL"
+    column_available_memory = "FREE_MEM_SIZE"
+    column_consistent_gets = "CONSISTENT_GETS"
+    # multi model test
+    predict1 = Predict(model_available_memory)
+    predict1.predict(filename_available_memory, column_available_memory, timesteps)
+    predict2 = Predict(model_consistent_gets)
+    predict2.predict(filename_consistent_gets, column_consistent_gets, timesteps)
+
     scheduler = BackgroundScheduler()
     scheduler.add_job(tick, 'interval', seconds=3)
     scheduler.add_job(tick1, 'interval', seconds=3)
