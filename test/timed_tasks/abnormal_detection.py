573f0cb95613465ebdaace0403ce7712df79be0f
Jianyu.Wang <873869027@qq.com>
2018-10-22 10:07:06 +0800

    apscheduler timed task control finished.

--- test/timed_tasks/abnormal_detection.py
+++ test/timed_tasks/abnormal_detection.py
@@ -1,47 +1,45 @@
-import tensorflow as tf
-from utils.preprocessing import Data
-from timed_tasks.predict import Predict
-
 import sys
 import time
-from datetime import datetime
+from timed_tasks.predict import Predict
 from apscheduler.schedulers.background import BackgroundScheduler
 
 # sys.path.append("") cmd need system path added
 
 
-def tick():
-    print('Tick! The time is: %s' % datetime.now())
+# all settings
+timesteps = 20
+batch_size = 64
+
+filename_available_memory = "../../datasets/db_os_stat(180123-180128).csv"
+filename_consistent_gets = "../../datasets/db_stat(180123-180128).csv"
+model_available_memory = "../../model/abnormal_detection_model/available_memory_model/FREE_MEM_SIZE_MODEL"
+model_consistent_gets = "../../model/abnormal_detection_model/consistent_gets_model/CONSISTENT_GETS_MODEL"
+column_available_memory = "FREE_MEM_SIZE"
+column_consistent_gets = "CONSISTENT_GETS"
+
+# multi model initial
+predict_available_memory = Predict(model_available_memory)
+predict_consistent_gets = Predict(model_consistent_gets)
 
 
-def tick1():
-    print('Tick! The time1 is: %s' % datetime.now())
+def predict_task():
+    print(time.strftime('start time: ' + '%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    # predict tasks
+    predict_available_memory.predict(filename=filename_available_memory,
+                                     column=column_available_memory,
+                                     timesteps=timesteps)
+    predict_consistent_gets.predict(filename=filename_consistent_gets,
+                                    column=column_consistent_gets,
+                                    timesteps=timesteps)
 
 if __name__ == '__main__':
-
-    # settings
-    timesteps = 20
-    batch_size = 64
-
-    filename_available_memory = "../../datasets/db_os_stat(180123-180128).csv"
-    filename_consistent_gets = "../../datasets/db_stat(180123-180128).csv"
-    model_available_memory = "../../model/abnormal_detection_model/available_memory_model/FREE_MEM_SIZE_MODEL"
-    model_consistent_gets = "../../model/abnormal_detection_model/consistent_gets_model/CONSISTENT_GETS_MODEL"
-    column_available_memory = "FREE_MEM_SIZE"
-    column_consistent_gets = "CONSISTENT_GETS"
-    # multi model test
-    predict1 = Predict(model_available_memory)
-    predict1.predict(filename_available_memory, column_available_memory, timesteps)
-    predict2 = Predict(model_consistent_gets)
-    predict2.predict(filename_consistent_gets, column_consistent_gets, timesteps)
-
     scheduler = BackgroundScheduler()
-    scheduler.add_job(tick, 'interval', seconds=3)
-    scheduler.add_job(tick1, 'interval', seconds=3)
+    scheduler.add_job(predict_task, 'interval', seconds=5)
+    # scheduler.add_job(predict_task_2, 'interval', seconds=3)
     scheduler.start()
 
     try:
         while True:
-            time.sleep(2)
+            time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         scheduler.shutdown()
